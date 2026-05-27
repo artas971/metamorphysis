2a612b587b7abf700fd58636da2e21ede5b5f94f
artas971 <steve.astasie@gmail.com>
2026-05-27 02:10:35 +0200

    Installation de EasyAdmin 5 et de la base de données

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Controller\Admin\PrestationCrudController;
+use App\Controller\Admin\UserCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+
+#[AdminDashboard(routePath: '/admin', routeName: 'admin')]
+class DashboardController extends AbstractDashboardController
+{
+    public function index(): Response
+    {
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(PrestationCrudController::class)->generateUrl());
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Metamorphysis - Espace Premium');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Tableau de bord', 'fa fa-home');
+        
+        yield MenuItem::section('Gestion du Cabinet');
+        
+        // CORRECTION EASYADMIN 5 : On utilise linkTo() avec le Controller en premier
+        yield MenuItem::linkTo(PrestationCrudController::class, 'Prestations', 'fas fa-list');
+        yield MenuItem::linkTo(UserCrudController::class, 'Utilisateurs', 'fas fa-users');
+        
+        yield MenuItem::section('Site Web');
+        yield MenuItem::linkToUrl('Retour au site', 'fas fa-globe', '/');
+    }
+}
