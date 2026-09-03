784929f1573bdfab9e3d18b8e5fab415c4227111
steve <artas971@gmail.com>
2026-09-03 12:04:16 +0200

    fix: Forcer la redirection HTTPS et trusted proxies pour éliminer l'avertissement formulaire non sécurisé

--- public/index.php
+++ public/index.php
@@ -1,8 +1,15 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+// Sur OVH et reverse proxies : faire confiance aux en-têtes HTTPS (X-Forwarded-Proto)
+Request::setTrustedProxies(
+    ['127.0.0.1', 'REMOTE_ADDR'],
+    Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO
+);
 
 return static function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
